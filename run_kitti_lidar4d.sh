73cfb5ea87026c95959437c8cf5ad9efaf19426c
Richard Marcus <richard.marcus@fau.de>
2025-04-10 12:44:17 +0200

    remove print

--- run_kitti_lidar4d.sh
+++ run_kitti_lidar4d.sh
@@ -54,7 +54,7 @@ CUDA_VISIBLE_DEVICES=0 python main_lidar4d.py \
 --path $path \
 --lr 1e-2 \
 --num_rays_lidar 1024 \
---iters 30000 \
+--iters 3000 \
 --alpha_d 1 \
 --alpha_i 0.1 \
 --alpha_r 0.01 \
